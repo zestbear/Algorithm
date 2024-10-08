0be2f52f92422a2c12c327a7fd1d7a6c4e07f416
KIM DONGHYUN <zestbear90@gmail.com>
2024-10-09 02:53:02 +0900

    [level 3] Title: 등굣길, Time: 0.73 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,41 +1,36 @@
 class Solution {
-    
-    static int dm;
-    static int dn;
-    
-    public static boolean check(int x, int y) {
-        if(0<=x && x<dm && 0<=y && y<dn) return true;
-        else return false;
-    }
-    
+
+    int dm;
+    int dn;
+
     public int solution(int m, int n, int[][] puddles) {
-        dm = m; dn = n;
+        dm = m;
+        dn = n;
         int[][] dp = new int[m][n];
         dp[0][0] = 1;
-        
+
         boolean[][] pud = new boolean[m][n];
-        for(int i=0;i<puddles.length;i++){
-            pud[puddles[i][0]-1][puddles[i][1]-1] = true;
+        for (int i = 0; i < puddles.length; i++) {
+            pud[puddles[i][0] - 1][puddles[i][1] - 1] = true;
         }
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                if(pud[i][j]) {
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                if (pud[i][j]) {
                     dp[i][j] = 0;
                     continue;
                 }
-                
-                int nx=0;
-                int ny=0;
-                if(check(i-1,j)) nx=dp[i-1][j];
-                if(check(i,j-1)) ny=dp[i][j-1];
-                
-                dp[i][j]=nx+ny;
-                dp[i][j]%=1000000007;
+
+                int nx = 0;
+                int ny = 0;
+                if (i > 0) nx = dp[i - 1][j];
+                if (j > 0) ny = dp[i][j - 1];
+
+                dp[i][j] = (nx + ny) % 1000000007;
             }
         }
-        
-        return dp[m-1][n-1];
+
+        return dp[m - 1][n - 1];
     }
 }
